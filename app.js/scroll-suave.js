5a28ff1550438a78d1506a8ac2406eb10200004c
carlossroliveira <63623377+carlossroliveira@users.noreply.github.com>
2020-10-16 10:05:11 -0300

    atualização
    16/10/2020

--- app.js/scroll-suave.js
+++ app.js/scroll-suave.js
@@ -3,7 +3,7 @@
             e.preventDefault();
             var id = $(this).attr('href'),
                 menuHeight = $('.menu').innerHeight(),
-                targetOffset = $(id).offset().top;
+                targetOffset = $(id).offset().top +540;
             $('html, body').animate({
                 scrollTop: targetOffset - menuHeight
             }, 500);
