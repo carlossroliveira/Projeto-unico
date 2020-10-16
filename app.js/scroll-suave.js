1031fab0b3d7e56c3689db15e811255782909539
carlossroliveira <63623377+carlossroliveira@users.noreply.github.com>
2020-10-16 11:07:46 -0300

    Update scroll-suave.js

--- app.js/scroll-suave.js
+++ app.js/scroll-suave.js
@@ -1,10 +1,24 @@
         // Scroll suave para link interno
-        $('.menu-nav a[href^="#"]').click(function (e) {
-            e.preventDefault();
-            var id = $(this).attr('href'),
-                menuHeight = $('.menu').innerHeight(),
-                targetOffset = $(id).offset().top +540;
-            $('html, body').animate({
-                scrollTop: targetOffset - menuHeight
-            }, 500);
-        });
+
+        var tamanho = $(window).width();
+        if (tamanho > 767) {
+            $('.menu-nav a[href^="#"]').click(function (e) {
+                e.preventDefault();
+                var id = $(this).attr('href'),
+                    menuHeight = $('.menu').innerHeight(),
+                    targetOffset = $(id).offset().top;
+                $('html, body').animate({
+                    scrollTop: targetOffset - menuHeight
+                }, 500);
+            });
+        } else {
+            $('.menu-nav a[href^="#"]').click(function (e) {
+                e.preventDefault();
+                var id = $(this).attr('href'),
+                    menuHeight = $('.menu').innerHeight(),
+                    targetOffset = $(id).offset().top + 580;
+                $('html, body').animate({
+                    scrollTop: targetOffset - menuHeight
+                }, 800);
+            });
+        }
